1bd11d4ebde2bf507f67582a60d45556d67f2c4e
iamtheasad <iamtheasad@gmail.com>
2019-11-27 13:48:59 +0600

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+//  Counter Up On Scrolling
+$('.count_test').countUp({
+  'time': 2000,
+  'delay': 10
+});
+
+// Isotop for services_2 section
+// $('.service_gallery_isotop').isotope({
+//   // options
+//   itemSelector: '.card',
+//   layoutMode: 'fitRows'
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
