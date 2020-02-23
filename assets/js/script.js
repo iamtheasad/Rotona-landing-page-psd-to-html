e4c525e9ca1477e3f763d729801dc967c61815a9
iamtheasad <iamtheasad@gmail.com>
2020-02-23 15:34:51 +0600

    markup, css ,js validated, bootstrap navbar added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,13 @@
+(function iamStrick() {
+  "use strict";
 
-//  Counter Up On Scrolling
-$('.count_test').countUp({
-  'time': 2000,
-  'delay': 10
-});
+  //  Counter Up On Scrolling
+  $('.count_test').countUp({
+    'time': 2000,
+    'delay': 10
+  });
 
-// Isotop for services_2 section
-// $('.service_gallery_isotop').isotope({
-//   // options
-//   itemSelector: '.card',
-//   layoutMode: 'fitRows'
-// });
+})();
 
 
 
